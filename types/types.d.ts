89b88890dd1a2fa327da63d1f786ae5c393ca373
Allan Jardine <allan@datatables.net>
2022-10-27 16:04:54 +0000

    Correct Nuget jQuery UI dependency + Fix multiple exports from styling files

--- types/types.d.ts
+++ types/types.d.ts
@@ -5,38 +5,3 @@ import Api from "datatables.net";
 
 export default Api;
 
-
-// Dist-DataTables-StateRestore-Bulma integration with Bulma exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bulma integration with Bulma exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bulma integration with Bulma exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bulma integration with Bulma exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
-
-// Dist-DataTables-StateRestore-Bulma integration with Bulma exports the DataTables API having
-// set default values to complete the ingeration.
-import Api from "datatables.net";
-
-export default Api;
-
