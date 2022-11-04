c07ea4de80f7af189b3dd3d27c507c59e282c531
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 11:44:55 +0000

    9272f0a5e26e4a21dab2a5d02a21ee486856a80a
    Sync to source repo @9272f0a5e26e4a21dab2a5d02a21ee486856a80a

--- js/stateRestore.bulma.js
+++ js/stateRestore.bulma.js
@@ -1,41 +1,6 @@
-
 /*! Bootstrap integration for DataTables' StateRestore
  * © SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( [''], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				// CommonJS environments without a window global must pass a
-				// root. This will give an error otherwise
-				root = window;
-			}
-
-			if ( ! $.fn.dataTable ) {
-				require('')(root, $);
-			}
-
-
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 $.extend(true, DataTable.StateRestoreCollection.classes, {
     checkRow: 'dtsr-check-row checkbox',
     creationButton: 'dtsr-creation-button button',
@@ -49,7 +14,3 @@ $.extend(true, DataTable.StateRestore.classes, {
     confirmationTitle: 'dtsr-confirmation-title modal-card-title',
     input: 'dtsr-input input'
 });
-
-
-return DataTable;
-}));
